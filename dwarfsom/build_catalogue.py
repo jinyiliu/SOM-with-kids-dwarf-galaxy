4ea85e4ddbf3a3d371c96a766c59444743933511
Jinyi Liu <liujy0129@gmail.com>
2026-02-04 19:00:08 +0100

    Move prevent_on_server function from utils to build_catalogue module

--- dwarfsom/build_catalogue.py
+++ dwarfsom/build_catalogue.py
@@ -5,8 +5,21 @@ import numpy as np
 
 from astropy.io import fits
 from astropy import table
+from functools import wraps
 
-from dwarfsom.utils import prevent_on_server
+def prevent_on_server(server_name: str="alblas"):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if os.uname().nodename.split(".")[0] == server_name:
+                raise EnvironmentError(
+                    f"Do not run on the node {server_name}. "
+                    f"This node has not enough memory."
+                )
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
 
 def Rhf2FWHM(Rhf):
     """Convert half-light radius to FWHM for a Gaussian profile."""
--- dwarfsom/utils.py
+++ dwarfsom/utils.py
@@ -1,24 +1,7 @@
 import os
 import numpy as np
-from functools import wraps
-
 from astropy import table
 from astropy.io import fits
-
-
-def prevent_on_server(server_name: str="alblas"):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            if os.uname().nodename.split(".")[0] == server_name:
-                raise EnvironmentError(
-                    f"Do not run on the node {server_name}. "
-                    f"This node has not enough memory."
-                )
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
-
 
 def fits2csv(
         fits_savepath: str,
