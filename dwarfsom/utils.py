93d388c32a9700a922d259693243c2464395472a
Jinyi Liu <liujy0129@gmail.com>
2026-02-04 18:51:36 +0100

    Add functions to retrieve tile sets for BASIC_RANDOMS and KiDS DR4

--- dwarfsom/utils.py
+++ dwarfsom/utils.py
@@ -43,3 +43,30 @@ def calculate_sky_area(ra_range: tuple[float], dec_range: tuple[float]):
     DeltaSinDec = np.sin(np.radians(dec_range[1])) - np.sin(np.radians(dec_range[0]))
     area = DeltaRA * np.degrees(DeltaSinDec)
     return area
+
+
+# The tile sets are different for BASIC RANDOMS and KiDS DR4.
+# KiDS DR4 has 1006 tiles, while BASIC RANDOMS has 1011 tiles.
+def get_BASIC_RANDOMS_tile_set() -> set[tuple[float, float]]:
+    """Get a set of BASIC_RANDOMS tile (ra, dec) centers."""
+    from dwarfsom.build_catalogue import _KiDS_RANDOMS_DIR
+    fnames = os.listdir(_KiDS_RANDOMS_DIR)
+    tiles = {
+        (
+            float(fname.split("_")[1].replace("p", ".")),
+            float(fname.split("_")[2].replace("p", ".").replace("m", "-"))
+         )
+        for fname in fnames
+    }
+    return tiles
+
+
+def get_KiDS_DR4_tile_set() -> set[tuple[float, float]]:
+    """Get a set of KiDS DR4 tile (ra, dec) centers."""
+    from dwarfsom.build_catalogue import _KiDS_TILE_DATA_DIR
+    fnames = os.listdir(_KiDS_TILE_DATA_DIR)
+    tiles = {
+        (float(fname.split("_")[2]), float(fname.split("_")[3]))
+        for fname in fnames
+    }
+    return tiles
