329bb6c268303da9c601bdfeb8f4a9ff34310a6d
Jinyi Liu <liujy0129@gmail.com>
2026-06-12 13:03:09 +0200

    Add critical_surface_density function to remove dsigma package dependence

--- dwarfsom/dsigma.py
+++ dwarfsom/dsigma.py
@@ -7,8 +7,9 @@ from astropy.io import fits
 from natsort import natsorted
 from dataclasses import dataclass
 from treecorr import NGCorrelation, Catalog
+from astropy import units as u
 from astropy.cosmology import FlatLambdaCDM, Planck18
-from dsigma.physics import critical_surface_density
+from astropy.cosmology import units as cu
 from deprecation import deprecated
 
 from dwarfsom.build_catalogue import KiDS_RANDOMS_DIR
@@ -325,3 +326,38 @@ def get_list_Random_catalogues(save_dir=KiDS_RANDOMS_DIR) -> list[Random]:
     return ret
 
 
+def critical_surface_density(
+        z_l, z_s, cosmology, comoving=True, d_l=None, d_s=None):
+    """Compute the critical surface density.
+
+    Copied from dsigma package:
+    https://github.com/johannesulf/dsigma/blob/main/dsigma/physics.py
+
+    Args:
+    z_l: Redshift of lens.
+    z_s: Redshift of source.
+    cosmology: Cosmology to assume for calculations. Only used if comoving
+        distances are not passed. If None, use dsigma.default_cosmology.
+        Default is None.
+    comoving: Flag for using comoving instead of physical units.
+        Default is True.
+    d_l: Comoving transverse distance to the lens. If not given, it is
+        calculated from the redshift provided. Default is None.
+    d_s: Comoving transverse distance to the source. If not given, it is
+        calculated from the redshift provided. Default is None.
+    """
+    if d_l is None:
+        d_l = cosmology.comoving_transverse_distance(z_l)
+    if d_s is None:
+        d_s = cosmology.comoving_transverse_distance(z_s)
+
+    with np.errstate(divide="ignore"):
+        sigma_crit = c.c**2 / (4 * np.pi * c.G) * (
+            (d_s / (1 + z_s)) / (d_l / (1 + z_l)) / ((d_s - d_l) / (1 + z_s)))
+    sigma_crit = np.where(d_s <= d_l, np.inf * sigma_crit.unit, sigma_crit)
+
+    if comoving:
+        sigma_crit /= (1.0 + z_l)**2
+
+    return sigma_crit.to(
+        cu.littleh * u.Msun / u.pc**2, cu.with_H0(cosmology.H0))
