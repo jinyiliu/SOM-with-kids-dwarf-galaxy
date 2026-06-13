3f441f610f6f9a0441f5296f3cf0d9b45f4afb73
Jinyi Liu <liujy0129@gmail.com>
2026-06-13 22:29:17 +0200

    Refactor MCMC class

--- dwarfsom/mcmc.py
+++ dwarfsom/mcmc.py
@@ -12,12 +12,11 @@ class MCMC:
             data_vector: np.ndarray,
             covariance_matrix: np.ndarray,
             param_priors: dict[str, list[str | tuple[float, float | np.ndarray]]] = None,
-            processes: int=4,
     ):
         self.sampler = None
         self.model = model
         self.dv = data_vector
-        self.processes = processes
+        self.ndim = len(param_priors)
 
         try:
             np.linalg.cholesky(covariance_matrix)
@@ -56,7 +55,7 @@ class MCMC:
 
         return log_prior
 
-    def _Gaussian_log_likelihood(self, params):
+    def Gaussian_log_likelihood(self, params):
         return -0.5 * (self.chi2(params)) + self._log_prior(params)
 
     def chi2(self, params):
@@ -67,7 +66,7 @@ class MCMC:
         chi2 = (self.dv - pred).T @ self.inv_cov @ (self.dv - pred)
         return chi2
 
-    def _get_random_walk(self, n_walkers: int):
+    def get_random_walk(self, n_walkers: int):
         """Generate a random walk for the MCMC."""
         p0 = np.zeros((n_walkers, self.n_dim))
 
@@ -87,47 +86,8 @@ class MCMC:
 
         return p0
 
-    def run_mcmc(
-            self,
-            n_walkers: int=100,
-            n_steps: int=500,
-            n_burn_in_steps: int=100,
-            progress: bool=True,
-            progress_kwargs=_tqdm_style,
-            **kwargs,
-    ):
-        """Run the MCMC sampler.
 
-        Args:
-            n_walkers: Number of walkers in the MCMC sampler.
-            n_steps: Number of steps in the MCMC sampler.
-            n_burn_in_steps: Number of burn-in steps to discard.
-            progress: Show a progress bar using tqdm.
-            progress_kwargs: Keyword arguments passed to tqdm.
-            kwargs: Additional keyword arguments for the
-                emcee.EnsembleSampler.sample method.
-        """
-        p0 = self._get_random_walk(n_walkers=n_walkers)
-
-        with Pool(processes=self.processes) as pool:
-            sampler = EnsembleSampler(
-                n_walkers, self.n_dim, self._Gaussian_log_likelihood, pool=pool,
-            )
-            state = sampler.run_mcmc(
-                initial_state=p0,
-                nsteps=n_burn_in_steps,
-                progress=progress,
-                progress_kwargs=progress_kwargs,
-            )
-            sampler.reset()
-            sampler.run_mcmc(
-                initial_state=state,
-                nsteps=n_steps,
-                progress=progress,
-                progress_kwargs=progress_kwargs,
-                **kwargs
-            )
-
+    def register_sampler(self, sampler):
         self.sampler = sampler
 
     def get_chain(self, flat=True):
@@ -147,6 +107,42 @@ class MCMC:
             raise ValueError("MCMC sampler has not been run yet.")
 
         np.save(fname, self.sampler.get_log_prob(flat=flat))
+
+
+def run_mcmc(
+        mcmc: MCMC,
+        n_walkers: int=100,
+        n_burn_in_steps: int=100,
+        n_steps: int=500,
+        processes: int=4,
+        progress: bool=True,
+        progress_kwargs=_tqdm_style,
+):
+    with Pool(processes=processes) as pool:
+        sampler = EnsembleSampler(
+            nwalkers=n_walkers,
+            ndim=mcmc.ndim,
+            log_prob_fn=mcmc.Gaussian_log_likelihood,
+            pool=pool,
+        )
+        state = sampler.run_mcmc(
+            initial_state=mcmc.get_random_walk(n_walkers=n_walkers),
+            nsteps=n_burn_in_steps,
+            progress=progress,
+            progress_kwargs=progress_kwargs,
+        )
+        sampler.reset()
+        sampler.run_mcmc(
+            initial_state=state,
+            nsteps=n_steps,
+            progress=progress,
+            progress_kwargs=progress_kwargs,
+        )
+    return sampler
+
+
+def random_likelihood(params):
+    return -0.5 * np.abs(np.random.normal(size=len(params)))
 
 
 def compute_quantiles(
